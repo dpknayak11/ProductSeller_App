3e0c6488129ff806df4d22d59a60edc587e77617
dpknayak11 <dpknayak111@gmail.com>
2023-10-10 20:02:06 +0530

    product seller app

---
+++ app.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require('express');
+const path = require('path');
+const sequelizedb = require('./model/products')
+const bodyParser = require("body-parser");
+const errorcontroller = require('./controller/404')
+const productPage = require('./routes/product')
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname,'public')))
+
+app.use(productPage);
+app.use(errorcontroller.get404);
+const port = process.env.PORT;
+
+sequelizedb
+.sync()
+// .sync({force: true})
+.then(() => { 
+   app.listen(port , ()=> console.log(`Listening on ${port}`));
+ })
+.catch((err) => { console.log(err) });
